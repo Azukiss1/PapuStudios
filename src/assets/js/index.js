a5ed20b81bdf0776333c5a01224a61652e99f0b4
Azukiss1 <151700145+Azukiss1@users.noreply.github.com>
2024-05-04 22:08:08 -0500

    2.0.0
    fix the bugs

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -29,9 +29,9 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "Je... vie...", "author": "Luuxis" },
-            { "message": "Salut je suis du code.", "author": "Luuxis" },
-            { "message": "Linux n'est pas un os, mais un kernel.", "author": "Luuxis" }
+            { "message": "Ant Studios god", "author": "??" },
+            { "message": "Cresiendo poco a poco.", "author": "??" },
+            { "message": "Si se puede", "author": "??" }
         ];
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
